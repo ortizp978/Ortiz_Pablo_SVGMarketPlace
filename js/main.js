60fe2203bc59b7995eff3e16b5aacb7638c4a4f2
Pablo Ortiz <pablortiz78@MacBook-Pro-de-Pablo.local>
2022-01-18 21:07:27 -0500

    adding SVG

--- js/main.js
+++ js/main.js
@@ -6,15 +6,20 @@
 console.log('this is some text');
 
 //go into the document and make a referrence to an element that you want to work with
-let theHeader = document.querySelector("#main-header");
+let theHeading = document.querySelector(".main-heading"),
+    theIcons = document.querySelectorAll(".icon");
 
 //create a function that runs when theHeader element is clicked
 function logElement() {
-    console.log('clicked on an element!');
+    console.log('clicked on an element: ', this.id);
 }
 
 //Listen for a user event, and then do something
 //the "do something" is the function we run when the element is clicked
-theHeader.addEventListener("click", logElement);
+theHeading.addEventListener("click", logElement);
+
+// loop through all of the icons and add event handling to them
+//process each icon one at a time and add behaviour
+theIcons.forEach(icon => icon.addEventListener("click", logElement));
 
 })();
